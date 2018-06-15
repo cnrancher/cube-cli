b002e1695f0ef6ad3444c7b44c8a32b27c953ff0
Jason-ZW <zhenyang@rancher.com>
2018-06-15 15:50:48 +0800

    Add server status logic

---
+++ docker/status.go
@@ -0,0 +1,44 @@
+package docker
+
+import (
+	"context"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/sirupsen/logrus"
+)
+
+func StatusContainer(ctx context.Context, dClient *client.Client, containerName string) (string, error) {
+	containers, err := dClient.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		logrus.Errorf("list docker containers error: %v", err)
+		return "", err
+	}
+
+	isFound := false
+	containerStatus := ""
+	// check that the container with the specified name exists
+	for _, c := range containers {
+		for _, name := range c.Names {
+			if strings.ContainsAny(name, containerName) {
+				isFound = true
+				containerStatus = c.Status
+				break
+			}
+		}
+		if isFound {
+			break
+		}
+	}
+
+	if isFound && containerStatus != "" {
+		// status the container
+		logrus.Info(containerStatus)
+		return containerStatus, nil
+	}
+
+	logrus.Warnf("container not found")
+
+	return "NotFound", nil
+}
