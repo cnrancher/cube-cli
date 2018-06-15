3f7ba8adcbf7b64e59b5300aabb618ad5ce03ec2
niusmallnan <niusmallnan@gmail.com>
2018-06-15 17:47:45 +0800

    Remove rke config and set RKE_CONFIG env

--- cmd/rke.go
+++ cmd/rke.go
@@ -1,8 +1,14 @@
 package cmd
 
 import (
+	"os"
+
 	rkecmd "github.com/rancher/rke/cmd"
 	"github.com/urfave/cli"
+)
+
+const (
+	RKEConfigDefault = "/var/lib/rancher/cube/rke_config.yml"
 )
 
 func RKECommand() cli.Command {
@@ -10,11 +16,16 @@ func RKECommand() cli.Command {
 		Name:        "rke",
 		Usage:       "Mapping the RKE commands",
 		Description: "Manage the RancherCUBE Kubernetes",
+		Before: func(c *cli.Context) error {
+			if os.Getenv("RKE_CONFIG") == "" {
+				os.Setenv("RKE_CONFIG", RKEConfigDefault)
+			}
+			return nil
+		},
 		Subcommands: []cli.Command{
 			rkecmd.UpCommand(),
 			rkecmd.RemoveCommand(),
 			rkecmd.VersionCommand(),
-			rkecmd.ConfigCommand(),
 			rkecmd.EtcdCommand(),
 		},
 	}
