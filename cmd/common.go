5aea0542eecaa03f18429c0feff84dfafd5ed015
Jason-ZW <zhenyang@rancher.com>
2018-06-16 22:52:22 +0800

    Add node add/rm command

--- cmd/common.go
+++ cmd/common.go
@@ -5,10 +5,14 @@ import (
 	"github.com/urfave/cli"
 )
 
-var (
+const (
 	APIServerKubeConfig    = "/var/lib/rancher/cube/kube-config.yml"
 	APIServerImage         = "cnrancher/cube-apiserver"
 	APIServerContainerName = "cube-apiserver"
+	APIServerPortDefault   = "9600"
+	RKEConfigDefault       = "/var/lib/rancher/cube/rke_base.yml"
+	NodeConfigDefault      = "/var/lib/rancher/cube/node_config.yml"
+	SSHKeyPathDefault      = "/home/rancher/.ssh/id_rsa"
 )
 
 func defaultAction(fn func(ctx *cli.Context) error) func(ctx *cli.Context) error {
--- cmd/node.go
+++ cmd/node.go
@@ -1,8 +1,16 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/cnrancher/cube-cli/util"
+	rkecmd "github.com/rancher/rke/cmd"
+
+	"github.com/rancher/types/apis/management.cattle.io/v3"
+	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
 
@@ -14,11 +22,14 @@ Example:
 	# List the Rancher Kubernetes Engine Nodes
 	$ cube node ls
 	# Add the Rancher Kubernetes Engine Node
-	$ cube node add <node>
+	$ cube node add --address <address> --roles worker,etcd --user rancher --ssh-key-path /home/rancher/.ssh/id_rsa
 	# Remove the Rancher Kubernetes Engine Node
-	$ cube node rm <node>
+	$ cube node rm --address <address>
 `
-	Node = "node"
+	Address    = "address"
+	Roles      = "roles"
+	User       = "user"
+	SSHKeyPath = "ssh-key-path"
 )
 
 func NodeCommand() cli.Command {
@@ -41,10 +52,26 @@ func NodeCommand() cli.Command {
 				Description: "Add the Rancher Kubernetes Engine Node",
 				Flags: []cli.Flag{
 					cli.StringFlag{
-						Name:  Node,
-						Usage: "Specify node name",
+						Name:  Address,
+						Usage: "Specify node address, e.g. x.x.x.x",
+					},
+					cli.StringFlag{
+						Name:  Roles,
+						Value: "controlplane,worker,etcd",
+						Usage: "Specify node roles",
+					},
+					cli.StringFlag{
+						Name:  User,
+						Value: "rancher",
+						Usage: "Specify node user",
+					},
+					cli.StringFlag{
+						Name:  SSHKeyPath,
+						Value: "/home/rancher/.ssh/id_rsa",
+						Usage: "Specify node ssh key path",
 					},
 				},
+
 				Action: defaultAction(nodeAdd),
 			},
 			{
@@ -53,8 +80,8 @@ func NodeCommand() cli.Command {
 				Description: "Remove the Rancher Kubernetes Engine Node",
 				Flags: []cli.Flag{
 					cli.StringFlag{
-						Name:  Node,
-						Usage: "Specify node name",
+						Name:  Address,
+						Usage: "Specify node name, e.g. x.x.x.x",
 					},
 				},
 				Action: defaultAction(nodeRm),
@@ -64,24 +91,134 @@ func NodeCommand() cli.Command {
 }
 
 func nodeLs(ctx *cli.Context) error {
-	fmt.Println("node list")
+	config := &v3.RancherKubernetesEngineConfig{}
+	var err error
+
+	if _, fErr := os.Stat(NodeConfigDefault); fErr != nil {
+		config, err = util.ReadRKEConfig(RKEConfigDefault)
+	} else {
+		config, err = util.ReadRKEConfig(NodeConfigDefault)
+	}
+
+	if err != nil {
+		logrus.Errorf("%v", err)
+		return err
+	}
+	logrus.Infof("%v", config)
 	return nil
 }
 
 func nodeAdd(ctx *cli.Context) error {
-	node := ctx.String(Node)
-	if "" == node {
-		return fmt.Errorf("cube node add: require %v", Node)
+	address := ctx.String(Address)
+	if "" == address {
+		return fmt.Errorf("cube node add: require %v", Address)
 	}
-	fmt.Println("node add")
-	return nil
+
+	roles := ctx.String(Roles)
+	rolesList := strings.Split(roles, ",")
+
+	user := ctx.String(User)
+	sshKeyPath := ctx.String(SSHKeyPath)
+
+	config := &v3.RancherKubernetesEngineConfig{}
+	var err error
+
+	if _, fErr := os.Stat(NodeConfigDefault); fErr != nil {
+		config, err = util.ReadRKEConfig(RKEConfigDefault)
+	} else {
+		config, err = util.ReadRKEConfig(NodeConfigDefault)
+	}
+
+	if err != nil {
+		logrus.Errorf("%v", err)
+		return err
+	}
+
+	if config.Nodes != nil && len(config.Nodes) > 0 {
+		for _, node := range config.Nodes {
+			if node.Address == address {
+				logrus.Warnf("cube node add: node already exist")
+				return nil
+			}
+		}
+	}
+
+	config.Nodes = append(config.Nodes, v3.RKEConfigNode{
+		Address:    address,
+		Role:       rolesList,
+		User:       user,
+		SSHKeyPath: sshKeyPath,
+	})
+
+	context := context.Background()
+	_, _, _, _, _, err = rkecmd.ClusterUp(context, config, nil, nil, nil, false, "", false, true)
+	if err != nil {
+		logrus.Errorf("cube node add: execute rke command error %v", err)
+		return err
+	}
+
+	err = util.WriteRKEConfig(config, NodeConfigDefault)
+	if err != nil {
+		logrus.Errorf("cube node add: write rke config file error %v", err)
+		return err
+	}
+
+	return err
 }
 
 func nodeRm(ctx *cli.Context) error {
-	node := ctx.String(Node)
-	if "" == node {
-		return fmt.Errorf("cube node add: require %v", Node)
+	address := ctx.String(Address)
+	if "" == address {
+		return fmt.Errorf("cube node remove: require %v", Address)
 	}
-	fmt.Println("node remove")
-	return nil
+
+	config := &v3.RancherKubernetesEngineConfig{}
+	var err error
+
+	if _, fErr := os.Stat(NodeConfigDefault); fErr != nil {
+		config, err = util.ReadRKEConfig(RKEConfigDefault)
+	} else {
+		config, err = util.ReadRKEConfig(NodeConfigDefault)
+	}
+
+	if err != nil {
+		logrus.Errorf("%v", err)
+		return err
+	}
+
+	foundIndex := -1
+	for index, node := range config.Nodes {
+		if node.Address == address {
+			foundIndex = index
+			break
+		}
+	}
+
+	if config.Nodes == nil || len(config.Nodes) <= 0 {
+		logrus.Warnf("cube node remove: no nodes in config file")
+		return nil
+	}
+
+	if len(config.Nodes) == 1 {
+		config.Nodes = []v3.RKEConfigNode{}
+	} else {
+		left := config.Nodes[0 : foundIndex-1]
+		right := config.Nodes[foundIndex+1:]
+		config.Nodes = util.MergeNodes(left, right)
+	}
+
+	context := context.Background()
+	_, _, _, _, _, err = rkecmd.ClusterUp(context, config, nil, nil, nil, false, "", false, true)
+	if err != nil {
+		logrus.Errorf("cube node remove: execute rke command error %v", err)
+		return err
+	}
+
+	err = util.WriteRKEConfig(config, NodeConfigDefault)
+	if err != nil {
+		logrus.Errorf("cube node remove: write rke config error %v", err)
+		return err
+	}
+
+	return err
 }
